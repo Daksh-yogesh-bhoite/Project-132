1a0496d7a7a2f2c69d617901bd10702d9ab20c81
Daksh-yogesh-bhoite <95295074+Daksh-yogesh-bhoite@users.noreply.github.com>
2023-02-13 18:01:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location="bedroom.html"
+}
+function bottles(){
+    window.location="bottles.html"
+}
+function desk(){
+    window.location="desk.html"
+}
+function fruitbasket(){
+    window.location="fruitbasket.html"
+}
+function tv(){
+    window.location="tv&ac.html"
+}
